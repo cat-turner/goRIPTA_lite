c88bef163953d13a7b7a0caf71de9cb25544a840
Jameson <jmarsenault@email.neit.edu>
2016-12-03 20:26:04 -0500

    Merge branch 'master' of https://github.com/GorgonsMaze/goRIPTA_lite into GorgonsMaze-master
    # Conflicts:
    #	work/index.html
    #	work/src/ride-app/ride-app.html

---
+++ work/src/ride-app/script/app.js
@@ -0,0 +1,14 @@
+/**
+ * Created by ian on 12/3/2016.
+ */
+
+(function () {
+    'use strict';
+
+
+    if ('serviceWorker' in navigator) {
+        navigator.serviceWorker
+            .register('./service-worker.js')
+            .then(function() { console.log('Service Worker Registered'); });
+    }
+})();
